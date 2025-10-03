f2179d4090727dc95c7ad4b7d2dd6325e54494ce
Poonam Sangale <149674027+Poonam-275@users.noreply.github.com>
2025-10-03 12:54:49 +0530

    Create auth.module.ts

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { FirebaseAuthStrategy } from './strategies/firebase-auth.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { UsersModule } from '../users/users.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '24h' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, FirebaseAuthStrategy, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
